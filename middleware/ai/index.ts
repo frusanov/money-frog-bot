fc54802a5f84c2b5a72e9d0b20c1364a8cc13521
Feodor Rusanov <f.rusanov@gmail.com>
2025-07-20 18:58:44 +0700

    feat: error handling

--- middleware/ai/index.ts
+++ middleware/ai/index.ts
@@ -6,27 +6,43 @@ import { contextHandler } from "../../utils/context-handler";
 export const debug = createDebug("ai-middleware");
 
 export const aiMiddleware: MiddlewareFn<Context> = (async (ctx, next) => {
-  let text = "";
-  let fileURLs: Array<URL> = [];
+  try {
+    let text = "";
+    let fileURLs: Array<URL> = [];
 
-  if (!ctx.has(message())) {
-    const {
-      message_id,
-      chat: { id },
-    } = ctx._preparedMessage;
-    ctx.telegram.editMessageText(id, message_id, undefined, "Error");
-    return next();
+    if (!ctx.has(message())) {
+      const {
+        message_id,
+        chat: { id },
+      } = ctx._preparedMessage;
+      ctx.telegram.editMessageText(id, message_id, undefined, "Error");
+      return next();
+    }
+
+    if (ctx.has(message("text"))) text = ctx.message.text;
+
+    if (ctx.has(message("photo"))) {
+      fileURLs = await Promise.all(
+        ctx.message.photo.map((photo) => {
+          return ctx.telegram.getFileLink(photo.file_id);
+        })
+      );
+    }
+
+    return contextHandler(ctx, text, fileURLs);
+  } catch (e) {
+    await ctx
+      .updatePreparedMessage(
+        `
+      En error occured:
+
+      ${(e as Error)?.message || "Unknown error"}
+
+      ${(e as Error)?.stack || ""}
+    `
+      )
+      .catch((e) => {
+        console.error(e);
+      });
   }
-
-  if (ctx.has(message("text"))) text = ctx.message.text;
-
-  if (ctx.has(message("photo"))) {
-    fileURLs = await Promise.all(
-      ctx.message.photo.map((photo) => {
-        return ctx.telegram.getFileLink(photo.file_id);
-      })
-    );
-  }
-
-  return contextHandler(ctx, text, fileURLs);
 }) as MiddlewareFn<Context>;
